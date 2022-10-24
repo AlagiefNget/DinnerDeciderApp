414c65dd00939a739de98bc38a3975820d70c139
Alagie F. Nget <Alagie10@gmail.com>
2022-10-23 22:49:37 -0500

    Setting new food text field to "" after adding a new food

--- app/src/main/java/com/example/dinnerdeciderapp/MainActivity.kt
+++ app/src/main/java/com/example/dinnerdeciderapp/MainActivity.kt
@@ -1,32 +1,43 @@
 package com.example.dinnerdeciderapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var addFoodButton: Button
+    lateinit var decideButton: Button
+    lateinit var newFood: EditText
+    lateinit var foodChoiceText: TextView
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addFoodButton = findViewById(R.id.addFood);
+        decideButton = findViewById(R.id.decide);
+        newFood = findViewById(R.id.newFood)
+        foodChoiceText = findViewById(R.id.choice);
     }
-
-//    private val textView: TextView = findViewById<TextView>(R.id.foodName)
-//    textView.setOnClickListener {
-//        textView.text = getString(R.string.name)
-//    }
-
-//    val str: String = textView.text.toString();
-
-
 
     var foodList  = arrayListOf<String>("Hamburger", "Pizza","Mexican", "American", "Chinese");
 
-    fun decide(view: View){
-
+    fun addFood(view: View){
+        val food = newFood.text.toString();
+        foodList.add(food);
+        foodChoiceText.text = food;
+        newFood.setText("");
     }
 
-    fun addFood(view: View){
-//        println("the value is $str");
+    fun decide(view: View){
+        val idx = (Math.random() * foodList.size).toInt();
+        val choice = foodList[idx];
+        foodChoiceText.text = choice;
     }
 }
